7caca378cadafd9a4d1c31d7b526b6bd42475611
Ben Burnes <ben.burnes@gmail.com>
2019-03-05 19:15:24 -0600

    Initial Unity commit.

---
+++ Assets/PuzzleBox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleBox:MonoBehaviour
+{
+  public Sprite Empty, Wrong, Marked;
+  private SpriteRenderer BoxSprite;
+  public Vector2Int BoxPosition { get; private set; }
+
+  private void Awake()
+  {
+    BoxSprite = GetComponent<SpriteRenderer>();
+  }
+
+  public void SetPosition(Vector2Int _boxPosition)
+  {
+    BoxPosition = _boxPosition;
+  }
+
+  public void SetState(int _state)
+  {
+    if (_state == 1)
+    {
+      BoxSprite.sprite = Marked;
+    }
+  }
+
+  public void SetColor(Color _color)
+  {
+    BoxSprite.color = _color;
+  }
+}
---
+++ Assets/PuzzleGameplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleGameplay : MonoBehaviour
+{
+  public PuzzleBox PuzzleBox;
+  private int[,] PuzzleGrid = new int[,]
+  {
+    {1,0,1,0,1},
+    {0,0,1,1,0},
+    {0,1,1,0,0},
+    {1,0,1,1,0},
+    {1,0,1,0,1}
+  };
+
+  private void Start()
+  {
+    for(int i=0; i<PuzzleGrid.GetLength(0); i++)
+    {
+      for(int j=0; j<PuzzleGrid.GetLength(1); j++)
+      {
+        PuzzleBox tempBox = Instantiate(PuzzleBox, this.transform);
+        tempBox.transform.localPosition = new Vector3(j, i, 0);
+        tempBox.SetPosition(new Vector2Int(j, i));
+        tempBox.SetState(PuzzleGrid[j, i]);
+      }
+    }
+  }
+}
